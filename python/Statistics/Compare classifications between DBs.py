348b9d7de789c1458f7cf25d02647d3438aecc11
hatzv <hatzvika@gmail.com>
2017-12-26 12:50:59 +0800

    Added a comparison between the DBs

--- python/Statistics/Compare classifications between DBs.py
+++ python/Statistics/Compare classifications between DBs.py
@@ -1,7 +1,7 @@
 from openpyxl import load_workbook, Workbook
 
 # Prepare the worksheet for the output comparison
-excel_filename_comparison = 'C:/Users/hatzv/Documents/Geography/RSTs/python/Statistics/Results/classification_comparison_1979-2016.xlsx'
+excel_filename_comparison = 'C:/Users/hatzv/Documents/Geography/RSTs/python/Statistics/Results/classification_comparison_between_DBs_1979-2016.xlsx'
 wb_comparison = Workbook()
 ws_comparison = wb_comparison.active
 table_comparison = ws_comparison['B2':'T15']
@@ -10,7 +10,6 @@ table_comparison = ws_comparison['B2':'T15']
 excel_filename_NCEP = 'C:/Users/hatzv/Documents/Geography/RSTs/python/Statistics/Results/RST_classification_NCEP_1979-2016.xlsx'
 excel_filename_ERA = 'C:/Users/hatzv/Documents/Geography/RSTs/python/Statistics/Results/RST_classification_ERA_1979-2016.xlsx'
 excel_filename_ERA_2_5 = 'C:/Users/hatzv/Documents/Geography/RSTs/python/Statistics/Results/RST_classification_ERA_2.5_1979-2016.xlsx'
-excel_filename_Isabella = 'C:/Users/hatzv/Documents/Geography/Research_help/synoptic_classification/My_classification/synoptic_classification_1948-21_Sep_2017.xlsx'
 
 wb_NCEP = load_workbook(excel_filename_NCEP, read_only=True)
 ws_NCEP = wb_NCEP.active
@@ -24,16 +23,11 @@ wb_ERA_2_5 = load_workbook(excel_filename_ERA_2_5, read_only=True)
 ws_ERA_2_5 = wb_ERA_2_5.active
 table_ERA_2_5 = ws_ERA_2_5['B2':'AM367']
 
-wb_Isabella = load_workbook(excel_filename_Isabella, read_only=True)
-ws_Isabella = wb_Isabella.active
-table_Isabella = ws_Isabella['AH2':'BS367']
-
 for row in range (ws_NCEP.max_row - 1):
     for col in range (ws_NCEP.max_column - 1):
         NCEP_value = table_NCEP[row][col].value
         ERA_value = table_ERA[row][col].value
         ERA_2_5_value = table_ERA_2_5[row][col].value
-        isabella_value = table_Isabella[row][col].value
 
         if NCEP_value == "No RST":
             output_row_NCEP = 0
@@ -62,24 +56,24 @@ for row in range (ws_NCEP.max_row - 1):
         elif ERA_2_5_value == "West":
             output_row_ERA_2_5 = 13
 
-        if isabella_value is not None:  # Make sure not to read the None value in a non-leap year
-            current_output_value_NCEP = table_comparison[output_row_NCEP][isabella_value-1].value
-            if current_output_value_NCEP is not None:
-                table_comparison[output_row_NCEP][isabella_value-1].value = current_output_value_NCEP +1
+        if NCEP_value is not None:  # Make sure not to read the None value in a non-leap year
+            current_output_value_NCEP_ERA = table_comparison[output_row_NCEP][output_row_ERA].value
+            if current_output_value_NCEP_ERA is not None:
+                table_comparison[output_row_NCEP][output_row_ERA].value = current_output_value_NCEP_ERA +1
             else:
-                table_comparison[output_row_NCEP][isabella_value-1].value = 1
+                table_comparison[output_row_NCEP][output_row_ERA].value = 1
 
-            current_output_value_ERA = table_comparison[output_row_ERA][isabella_value - 1].value
-            if current_output_value_ERA is not None:
-                table_comparison[output_row_ERA][isabella_value - 1].value = current_output_value_ERA + 1
+            current_output_value_NCEP_ERA_2_5 = table_comparison[output_row_NCEP][output_row_ERA_2_5].value
+            if current_output_value_NCEP_ERA_2_5 is not None:
+                table_comparison[output_row_NCEP][output_row_ERA_2_5].value = current_output_value_NCEP_ERA_2_5 +1
             else:
-                table_comparison[output_row_ERA][isabella_value - 1].value = 1
+                table_comparison[output_row_NCEP][output_row_ERA_2_5].value = 1
 
-            current_output_value_ERA_2_5 = table_comparison[output_row_ERA_2_5][isabella_value - 1].value
-            if current_output_value_ERA_2_5 is not None:
-                table_comparison[output_row_ERA_2_5][isabella_value - 1].value = current_output_value_ERA_2_5 + 1
+            current_output_value_ERA_ERA_2_5 = table_comparison[output_row_ERA][output_row_ERA_2_5].value
+            if current_output_value_ERA_ERA_2_5 is not None:
+                table_comparison[output_row_ERA][output_row_ERA_2_5].value = current_output_value_ERA_ERA_2_5 +1
             else:
-                table_comparison[output_row_ERA_2_5][isabella_value - 1].value = 1
+                table_comparison[output_row_ERA][output_row_ERA_2_5].value = 1
 
 wb_comparison.save(excel_filename_comparison)
 
