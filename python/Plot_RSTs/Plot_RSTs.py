b6db5d0a0de4cccdba48b589ec1ef02a6f2d1cf2
hatzv <hatzvika@gmail.com>
2017-12-27 11:53:44 +0800

    Added composite maps for the new algorithm classification vs. Isabella's calssification.

--- python/Plot_RSTs/Plot_RSTs.py
+++ python/Plot_RSTs/Plot_RSTs.py
@@ -183,7 +183,7 @@ class PlotRSTs ():
         if not self.is_rst_condition_met:
             self.daily_rst_classification = consts.rst_orientation_no_rst
 
-        return self.daily_rst_classification
+        return self.daily_rst_classification, slp_data, self.geostrophic_vorticity_map
 
     def _calculate_troughs_and_ridges_dots(self, slp_data, total_lat, total_lon):
         troughs_map = np.zeros((total_lat, total_lon))
